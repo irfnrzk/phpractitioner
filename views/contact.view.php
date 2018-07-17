15f21670a07452493b97c2d59154e81d66e588b0
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2018-07-17 10:59:58 +0800

    Partial views

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,11 +1,3 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Contact</title>
-</head>
-<body>
+<?php require('partials/header.php'); ?>
   
-</body>
-</html>
+<?php require('partials/footer.php'); ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,15 +1,5 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>   
-  <ul>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
-  </ul>
+<?php require('partials/header.php'); ?>
+
   <h1>Task</h1>
   <ul>
       <?php foreach ($tasks as $task) : ?>
@@ -20,5 +10,5 @@
         <?php endif; ?>
       <?php endforeach; ?>
   </ul>
-</body>
-</html>
+
+<?php require('partials/footer.php'); ?>
